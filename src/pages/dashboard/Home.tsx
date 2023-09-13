c4462b059b4069afef63115fe3a3e4968bd2c077
Jamiu Ibikunle <ibidotun@gmail.com>
2023-09-13 10:38:05 +0100

    dashboard completed

--- src/pages/dashboard/Home.tsx
+++ src/pages/dashboard/Home.tsx
@@ -1,8 +1,11 @@
 import {
   Box,
+  Button,
   Center,
   HStack,
   Heading,
+  Image,
+  ScrollView,
   Text,
   VStack,
   View,
@@ -13,24 +16,69 @@ import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
 const Home = () => {
   return (
-    <View flex={1} bg="white" p="$5">
-      <HStack alignItems="center" gap="$3">
-        <Center
-          bg="rgba(212, 46, 18, 0.25)"
-          h="$16"
-          w="$16"
-          borderRadius="$full">
-          <Ionicons color="#d42e12" name="person-outline" size={22} />
-        </Center>
-        <VStack flex={1}>
-          <Heading>Ahmad Bashir</Heading>
-          <Text>41'24'12.2"N 2'10'26.5"E</Text>
-        </VStack>
-        <Box alignSelf="flex-start">
-          <SimpleLineIcons color="#d42e12" size={28} name="bell" />
-        </Box>
-      </HStack>
-    </View>
+    <ScrollView style={{flex: 1, backgroundColor: 'white'}}>
+      <VStack gap="$4" flex={1} p="$5">
+        <HStack alignItems="center" gap="$3">
+          <Center
+            bg="rgba(212, 46, 18, 0.25)"
+            h="$16"
+            w="$16"
+            borderRadius="$full">
+            <Ionicons color="#d42e12" name="person-outline" size={22} />
+          </Center>
+          <VStack flex={1}>
+            <Heading>Ahmad Bashir</Heading>
+            <Text>41'24'12.2"N 2'10'26.5"E</Text>
+          </VStack>
+          <Box alignSelf="flex-start">
+            <SimpleLineIcons color="#d42e12" size={28} name="bell" />
+          </Box>
+        </HStack>
+        <Button variant="link" alignSelf="flex-start">
+          <Text color="#d42e12">Report Emergency</Text>
+        </Button>
+        <HStack flexWrap="wrap" justifyContent="space-between" rowGap="$8">
+          <Item
+            image={require('../../../public/health.png')}
+            title="Health Emergency"
+          />
+          <Item
+            image={require('../../../public/fire.png')}
+            title="Fire Emergency"
+          />
+          <Item
+            image={require('../../../public/police.png')}
+            title="Health Emergency"
+          />
+          <Item
+            image={require('../../../public/more.png')}
+            title="Fire Emergency"
+          />
+        </HStack>
+        <Image
+          source={require('../../../public/map-placeholder.png')}
+          w="100%"
+        />
+      </VStack>
+    </ScrollView>
+  );
+};
+
+const Item = ({image, title}: {image: any; title: string}) => {
+  return (
+    <VStack
+      w="48%"
+      h="$40"
+      justifyContent="center"
+      alignItems="center"
+      gap="$5"
+      p="$5"
+      borderRadius="$md"
+      borderWidth="$1"
+      borderColor="$backgroundDark200">
+      <Image source={image} />
+      <Text>{title}</Text>
+    </VStack>
   );
 };
 
