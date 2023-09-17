37b1a359f783d37a837cca05b57bfeab7d932695
Jamiu Ibikunle <ibidotun@gmail.com>
2023-09-17 11:25:49 +0100

    changed overlay

--- src/components/ModalBody.tsx
+++ src/components/ModalBody.tsx
@@ -1,7 +1,7 @@
 import {Box, Button, Center, Text, VStack, View} from '@gluestack-ui/themed';
 import React, {useEffect} from 'react';
 import Entypo from 'react-native-vector-icons/Entypo';
-import {Modal} from 'react-native';
+import {Modal, TouchableOpacity} from 'react-native';
 
 const ModalBody = ({
   children,
@@ -12,20 +12,22 @@ const ModalBody = ({
 }) => {
   return (
     <Modal animationType="fade" transparent={true} visible={true}>
-      <Center h="100%" w="100%" backgroundColor="rgba(237, 242, 247, 0.7)">
-        <VStack
-          alignItems="center"
-          w="80%"
-          p="$3"
-          pb="$8"
-          bg="white"
-          borderRadius="$lg">
-          <Button onPress={onClose} variant="link" alignSelf="flex-end">
-            <Entypo name="cross" size={30} color="#d42e12" />
-          </Button>
-          <Box w="$full">{children}</Box>
-        </VStack>
-      </Center>
+      <TouchableOpacity style={{backgroundColor: 'rgba(237, 242, 247, 0.7)'}}>
+        <Center h="100%" w="100%">
+          <VStack
+            alignItems="center"
+            w="80%"
+            p="$3"
+            pb="$8"
+            bg="white"
+            borderRadius="$lg">
+            <Button onPress={onClose} variant="link" alignSelf="flex-end">
+              <Entypo name="cross" size={30} color="#d42e12" />
+            </Button>
+            <Box w="$full">{children}</Box>
+          </VStack>
+        </Center>
+      </TouchableOpacity>
     </Modal>
   );
 };
