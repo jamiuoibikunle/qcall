fa9a99a835cf549a1ceac7ef5b2027a498782225
Jamiu Ibikunle <ibidotun@gmail.com>
2023-09-22 22:07:04 +0100

    updated modal blue

--- src/components/ModalBody.tsx
+++ src/components/ModalBody.tsx
@@ -2,6 +2,8 @@ import {Box, Button, Center, Text, VStack, View} from '@gluestack-ui/themed';
 import React, {useEffect} from 'react';
 import Entypo from 'react-native-vector-icons/Entypo';
 import {Modal, TouchableOpacity} from 'react-native';
+import {BlurView} from '@react-native-community/blur';
+import {StyleSheet} from 'react-native';
 
 const ModalBody = ({
   children,
@@ -12,22 +14,28 @@ const ModalBody = ({
 }) => {
   return (
     <Modal animationType="fade" transparent={true} visible={true}>
-      <TouchableOpacity style={{backgroundColor: 'rgba(237, 242, 247, 0.7)'}}>
-        <Center h="100%" w="100%">
-          <VStack
-            alignItems="center"
-            w="80%"
-            p="$3"
-            pb="$8"
-            bg="white"
-            borderRadius="$lg">
-            <Button onPress={onClose} variant="link" alignSelf="flex-end">
-              <Entypo name="cross" size={30} color="#d42e12" />
-            </Button>
-            <Box w="$full">{children}</Box>
-          </VStack>
-        </Center>
-      </TouchableOpacity>
+      <BlurView
+        style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
+        blurType="light"
+        blurAmount={1}
+        reducedTransparencyFallbackColor="white"
+      />
+      <Center h="100%" w="100%">
+        <VStack
+          borderWidth={1}
+          borderColor="$borderDark400"
+          alignItems="center"
+          w="80%"
+          p="$3"
+          pb="$8"
+          bg="white"
+          borderRadius="$lg">
+          <Button onPress={onClose} variant="link" alignSelf="flex-end">
+            <Entypo name="cross" size={30} color="#d42e12" />
+          </Button>
+          <Box w="$full">{children}</Box>
+        </VStack>
+      </Center>
     </Modal>
   );
 };
